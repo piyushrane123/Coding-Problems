79dee9ef16129dce5246a6fac0e8a51667f9f520
piyushrane123 <51693799+piyushrane123@users.noreply.github.com>
2019-06-12 00:01:27 +0530

    Create  SPOJ_Life, the Universe, and Everything.cpp

---
+++ SPOJ_Life, the Universe, and Everything.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+int a=0;
+
+while(a!=42)
+{ 
+   cin>>a;
+   if(a==42)
+   break;
+   cout<<a;
+  cout<<"\n";
+}
+   
+
+    
+
+	return 0;
+}
